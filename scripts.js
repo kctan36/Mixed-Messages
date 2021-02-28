2a91f52ec86d5dee7227c60e82fa849b7f678776
kctan36 <kctan0814@gmail.com>
2021-02-28 12:36:53 +0000

    Added message blocks and random number generator

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
 // Motivational Messages
 // Structure: Greeting, Message, Ending. 
 // E.g. "Hey there! You're doing AMAZING. Keep it up :) " 
-// Greetings: "Hey there!", "HEYAAAAA!" "~Greeting Summoner~"
-// Message: 
+// Greetings: "Hey there!", "Heyaaaa" "~Greetings Summoner~"
+// Message: "You only fail when you stop trying!", "Don't stop until youre proud", "Good things take time."
+// Ending: "Keep it up :)", "Until next time :D", "Have a fantastic day!"
+
+const greetng = ["Hey there!", "Heyaaaa!", "~Greetings Summoner~"];
+const message = ["You only fail when you stop trying!", "Don't stop until you're proud.", "Good things take time."];
+const ending = ["Keep it up :)", "Until next time :D", "Have a fantastic day!"];
+
+const randomNumber = list => Math.floor(Math.random() * list.length);
+
+const makeMix = () => {
+    g
+}
