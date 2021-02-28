e7c3f8b9d9157e7d92cfcabb12e114fd7050086f
kctan36 <kctan0814@gmail.com>
2021-02-28 12:59:09 +0000

    Finished final testing and the script works1

--- scripts.js
+++ scripts.js
@@ -10,14 +10,15 @@ const mixMessage = {
     message: ["You only fail when you stop trying!", "Don't stop until you're proud.", "Good things take time."],
     ending: ["Keep it up :)", "Until next time :D", "Have a fantastic day!"],
 
-    randomNumber(list){Math.floor(Math.random() * this.list.length)},
+    randomNumber(list){return Math.floor(Math.random() * list.length)},
 
     makeMix(){
         temp = [];
-        temp.push(greeting[this.randomNumber(greeting)]);
-        temp.push(message[this.randomNumber(message)]);
-        temp.push(ending[this.randomNumber(ending)]);
+        temp.push(this.greeting[this.randomNumber(this.greeting)]);
+        temp.push(this.message[this.randomNumber(this.message)]);
+        temp.push(this.ending[this.randomNumber(this.ending)]);
         return temp.join("\n");
     }
 }
 
+console.log(mixMessage.makeMix());
