a1eea2db0fcc1feacfed2497b8fb164718b63d30
kctan36 <kctan0814@gmail.com>
2021-02-28 11:52:43 +0000

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Motivational Messages
+// Structure: Greeting, Message, Ending. 
+// E.g. "Hey there! You're doing AMAZING. Keep it up :) " 
+// Greetings: "Hey there!", "HEYAAAAA!" "~Greeting Summoner~"
+// Message: 
